856b806d6810bda011bd835fdb751b30f73d9a2c
aska <j_miastkowska@o2.pl>
2024-01-10 11:00:38 +0100

    init new project

---
+++ tests/cart.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://skleptest.pl/');
+  await page.locator('#tyche_products-1').getByRole('link', { name: ' Add to cart' }).first().click();
+  await page.locator('#tyche_products-1').getByRole('link', { name: ' Add to cart' }).nth(1).click();
+  await page.getByRole('link', { name: ' My Cart - zł' }).click();
+  await page.getByRole('heading', { name: 'Cart', exact: true }).click();
+});

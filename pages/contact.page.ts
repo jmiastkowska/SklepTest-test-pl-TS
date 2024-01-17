cd8c6f912dab5d4775d0c20e208bd574e13d1b3b
aska <j_miastkowska@o2.pl>
2024-01-17 10:39:43 +0100

    fixed contact test

--- pages/contact.page.ts
+++ pages/contact.page.ts
@@ -9,6 +9,7 @@ export class ContactPage {
     messageInput = this.page.getByLabel('Your Message');
   
     sendButton = this.page.getByRole('button', { name: 'Send' });
-  
-  
+    
+    requiredEmailText = this.page.locator('//*[@class="wpcf7-not-valid-tip"]');
+    errorMessageAfterClickingSend = this.page.locator('//*[@id="wpcf7-f4-p10-o1"]/form/div[2]');
   }
--- tests/contact.spec.ts
+++ tests/contact.spec.ts
@@ -1,34 +1,49 @@
-import { test, expect } from "@playwright/test";
-import { DashboardPage } from "../pages/dashboard.page";
-import { ContactPage } from "../pages/contact.page";
+import { test, expect } from '@playwright/test';
+import { DashboardPage } from '../pages/dashboard.page';
+import { ContactPage } from '../pages/contact.page';
 
 test.describe('tests sending messages', () => {
-    let contactPage: ContactPage;
-    
-    test.beforeEach(async ({ page }) => {
-      await page.goto('/');
-      const dashboardPage = new DashboardPage(page);
-      
-      contactPage = new ContactPage(page);
-    });
-  
-    test('send message with correct data', async ({ page }) => {
-      const dashboardPage = new DashboardPage(page);
-      const name = 
-        await dashboardPage.sideMenu.aboutUsButton.click();
-        await contactPage.nameInput.fill();
-       // await cartPage.updateCartButton.click();
-  
-        //  await expect(cartPage.quantity).toHaveValue('2');
-        await page.getByLabel('Your Name (required)').click();
-        await page.getByLabel('Your Name (required)').fill('tester1');
-        await page.getByLabel('Your Email (required)').click();
-        await page.getByLabel('Your Email (required)').fill('testtest@wp.pl');
-        await page.getByLabel('Subject').click();
-        await page.getByLabel('Subject').fill('testuj');
-        await page.getByLabel('Your Message').click();
-        await page.getByLabel('Your Message').fill('jakas wiadomosc');
-        await page.getByRole('button', { name: 'Send' }).click();
-    });
+  let contactPage: ContactPage;
 
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+    const dashboardPage = new DashboardPage(page);
+    await dashboardPage.sideMenu.aboutUsButton.click();
+
+    contactPage = new ContactPage(page);
+  });
+
+  test('send message with correct data', async ({ page }) => {
+    const dashboardPage = new DashboardPage(page);
+    const name = 'Joanna';
+    const email = 'test12345@wp.pl';
+    const subject = 'hello';
+    const message = 'Please give me an answer';
+
+    await contactPage.nameInput.fill(name);
+    await contactPage.emailInput.fill(email);
+    await contactPage.subjectInput.fill(subject);
+    await contactPage.messageInput.fill(message);
+    await contactPage.sendButton.click();
+
+    await expect(contactPage.errorMessageAfterClickingSend).toHaveText(
+      'There was an error trying to send your message. Please try again later.',
+    );
+  });
+
+  test('send message with empty email', async ({ page }) => {
+    const dashboardPage = new DashboardPage(page);
+    const name = 'Joanna';
+    const subject = 'hello';
+    const message = 'Please give me an answer';
+    const expectedMessage = 'The field is required.';
+
+    await dashboardPage.sideMenu.aboutUsButton.click();
+    await contactPage.nameInput.fill(name);
+    await contactPage.subjectInput.fill(subject);
+    await contactPage.messageInput.fill(message);
+    await contactPage.sendButton.click();
+
+    await expect(contactPage.requiredEmailText).toHaveText(expectedMessage);
+  });
 });

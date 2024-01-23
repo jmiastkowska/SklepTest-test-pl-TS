286dc3e0a23867cd1a73f5cb48e2d51042c2a426
aska <j_miastkowska@o2.pl>
2024-01-23 19:52:02 +0100

    fixed test adding product on the cart

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -13,20 +13,22 @@ test.describe('tests cart page', () => {
       cartPage = new CartPage(page);
     });
   
-    test.only('add 1 product on the cart', async ({ page }) => {
+    test.only('add to the cart 1 product on the cart page', async ({ page }) => {
      
-      
+      await page.reload();
       await cartPage.plusButton.click();
       await cartPage.updateCartButton.click();
       
       await expect(cartPage.quantity).toHaveValue('2');
       await expect(cartPage.updateCartMessage).toHaveText('Cart updated.');
        const unitPriceTxt = await page.locator('//td[@class="product-price"]/span').innerText();
-       const unitPrice = unitPriceTxt.replace(' zł','');
-      //const receviedSubtotalPrice = Number(subtotalPrice.replace(' zł', ''));
-      const  expectedSubtotalPrice = Number(unitPrice);
-      const subtotalPrice = await page.locator('//*[@id="post-6"]/div[2]/form/table/tbody/tr[1]/td[6]/span').innerText();
-        await expect(subtotalPrice).toContainEqual(expectedSubtotalPrice);
+       const unitPrice = unitPriceTxt.replace('zł','');
+       const  expectedSubtotalPrice = (+unitPrice) *2;
+       
+       const subtotalPrice = await page.locator('//*[@id="post-6"]/div[2]/form/table/tbody/tr[1]/td[6]/span').innerText();
+       const receivedSubtotalPrice = +subtotalPrice.replace('zł', '');
+
+    await expect(receivedSubtotalPrice).toBe(expectedSubtotalPrice);
     });
     
     test('remove the one product from the cart', async ({ page }) => {
@@ -48,4 +50,15 @@ test.describe('tests cart page', () => {
       await expect(page).toHaveURL(/.shop/);
       
   });
+
+  test('check shipping price', async ({page}) => {
+await page.getByRole('link', { name: 'Calculate shipping' }).click();
+
+await page.getByRole('textbox', { name: 'Poland' }).click();
+
+await page.getByRole('combobox').nth(2).fill('german');
+await page.getByRole('option', { name: 'Germany' }).click();
+
+await page.getByPlaceholder('Postcode / ZIP').fill('12345');
+await page.getByRole('button', { name: 'Update totals' }).click();  });
 });

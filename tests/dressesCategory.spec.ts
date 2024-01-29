5cbb7bfaf188ad2dfc4e7cef8657ad7c0268a64f
aska <j_miastkowska@o2.pl>
2024-01-29 14:03:35 +0100

    only dress on the page

--- tests/dressesCategory.spec.ts
+++ tests/dressesCategory.spec.ts
@@ -14,7 +14,20 @@ test.describe('tests dresses category', () => {
   });
 
   test('open the categories dresses from the dashboard', async ({ page }) => {
- 
     await expect(page).toHaveURL(/.dresses/);
+  });
+
+  test('check if only dresses are on the dresses category', async ({ page }) => {
+   
+    const allLocatorsWithNameDress = page.locator('//*[@class="woocommerce-loop-product__title"]').evaluateAll((elements) =>
+    elements.map((element) => element.textContent?.toLowerCase()));
+
+  // Sprawdź, czy żaden z lokatorów nie zawiera słowa "dress"
+  for (const locatorText of await allLocatorsWithNameDress) {
+    expect(locatorText).toContain('dress');
+  }
+  expect((await allLocatorsWithNameDress).every((locatorText) => (locatorText).includes('dress'))).toBeTruthy();
+   // getByRole('link', { name: 'Marcara Sleeveless Dress 55 zł' })
+    //await expect(page).toHaveURL(/.dresses/);
   });
 });

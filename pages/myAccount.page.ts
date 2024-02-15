3ba74c58cb9aeb081aeb9b9b05d6b6bff2a86c66
aska <j_miastkowska@o2.pl>
2024-02-15 18:51:12 +0100

    up

--- pages/myAccount.page.ts
+++ pages/myAccount.page.ts
@@ -7,7 +7,7 @@ export class MyAccountPage {
   emailRegisterInput = this.page.locator('//*[@id="reg_email"]');
   passwordRegisterInput = this.page.locator('#reg_password');
   registerButton = this.page.getByRole('button', { name: 'Register' });
-  UserNameText = this.page.getByText('truskawka2').first();
+  UserNameText = this.page.locator('//*[@class="woocommerce-MyAccount-content"]');
 
 
   usernameInput = this.page.getByLabel('Username or email address *');
--- tests/my-account.spec.ts
+++ tests/my-account.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { DashboardPage } from '../pages/dashboard.page';
 import { MyAccountPage } from '../pages/myAccount.page';
+import { TIMEOUT } from 'dns';
 
 
 test.describe('tests login and register', () => {
@@ -16,18 +17,13 @@ test.describe('tests login and register', () => {
   
     test('register new account', async ({ page }) => {
       
-      await myAccountPage.emailRegisterInput.fill('truskawka4@wp.pl');
-     
-      await myAccountPage.passwordRegisterInput.fill('passTest123.');
+      await myAccountPage.emailRegisterInput.fill('truskawka9@wp.pl');
+      await myAccountPage.passwordRegisterInput.fill('passTestowe12345,.');
       await myAccountPage.registerButton.click();
-      await myAccountPage.registerButton.click();
-     await expect(myAccountPage.UserNameText).toHaveText('truskawka4');
+     await expect(myAccountPage.UserNameText).toHaveText('truskawka9');
     });
    
     test('login to account', async ({ page }) => {
-    
-     
-       
         await myAccountPage.passwordRegisterInput.fill('passTest123.');
         await myAccountPage.registerButton.click();
         await page.getByText('truskawka2').first().click();
